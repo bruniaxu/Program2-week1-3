f4abc768584b627e49598703ffd6c890cb75a835
Bruni Tang <bruniaxu@gmail.com>
2026-05-24 13:26:09 -0400

    testing to see if i can use the codes from other scripts from pervious weeks to make this works (Ducky movement)

---
+++ Assets/scripts/DuckyIdle.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class DuckyIdle : MonoBehaviour
+{
+    public Vector3 hidePosition;
+    public float hideDistanceThreshold;
+    public Camera gameCamera;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 mousePosition = Mouse.current.position.ReadValue();
+        Vector3 worldMousePosition = gameCamera.ScreenToWorldPoint(mousePosition);
+        worldMousePosition.z = 0f;
+
+        float distance = Vector3.Distance(mousePosition, transform.position);
+
+        Debug.Log(distance);
+
+        //ENTER THIS SECTION OF CODE:
+        //If the mouse is close to this object
+        if (distance < 0.25f)
+        {
+            transform.position = hidePosition;
+        }
+    }
+}
--- Assets/scripts/DuckyMovement.cs
+++ Assets/scripts/DuckyMovement.cs
@@ -1,16 +1,71 @@
 using UnityEngine;
+using static UnityEngine.RuleTile.TilingRuleOutput;
 
 public class DuckyMovement : MonoBehaviour
 {
+    public float xSpeed;
+    public float ySpeed;
+
+    //xMax
+    //xMin
+    public float xMax;
+    public float xMin;
+    //yMAx
+    //yMin
+    public float yMax;
+    public float yMin;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
-    }
+     
+}
 
     // Update is called once per frame
     void Update()
     {
-        
+    Vector3 newPosition = transform.position;
+
+    //When assigning a value to a float, we want to append "f" to the end
+    newPosition.x += xSpeed * Time.deltaTime;
+    newPosition.y += ySpeed * Time.deltaTime;
+
+    transform.position = newPosition;
+
+    if (newPosition.x > xMax)
+    {
+        //option 1
+        //xSpeed = -xSpeed;
+        //option 2
+        xSpeed *= -1f;
+
     }
+
+    if (newPosition.x < xMin)
+    {
+        //option 1
+        //xSpeed = -xSpeed;
+        //option 2
+        xSpeed *= -1f;
+
+    }
+
+    if (newPosition.y > yMax)
+    {
+        //option 1
+        //ySpeed = -ySpeed;
+        //option 2
+        ySpeed *= -1f;
+
+    }
+
+    if (newPosition.y < yMin)
+    {
+        //option 1
+        //ySpeed = -ySpeed;
+        //option 2
+        ySpeed *= -1f;
+
+    }
+}
 }
