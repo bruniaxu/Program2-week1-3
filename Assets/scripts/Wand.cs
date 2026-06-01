3a0431a4b5d6d6041379bd3099a43932b82b4105
Bruni Tang <bruniaxu@gmail.com>
2026-06-01 16:28:21 -0400

    added comments for wand scripts

--- Assets/scripts/Wand.cs
+++ Assets/scripts/Wand.cs
@@ -3,6 +3,8 @@ using UnityEngine.InputSystem;
 
 public class Wand : MonoBehaviour
 {
+    //create public variable
+    //set camera to make the wand visible on the game view
     public Camera gameCamera;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -14,10 +16,13 @@ public class Wand : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //get current mouse position 
         Vector3 currentMousePosition = Mouse.current.position.ReadValue();
-
+        //convert screen to world position coordinates
         Vector3 worldMousePosition = gameCamera.ScreenToWorldPoint(currentMousePosition);
+        //the project is 2d so this make the position stays on the same 2d plane
         worldMousePosition.z = 0f;
+        //move the wand to the mouse position, transdorm.position is used to move the object
         transform.position = worldMousePosition;
     }
 }
