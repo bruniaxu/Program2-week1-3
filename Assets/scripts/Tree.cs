4118a1f29a35380dd5bc85f115a6c179ed908ca3
Bruni Tang <bruniaxu@gmail.com>
2026-05-31 22:21:12 -0400

    added script for tree for bouncing effect (it looks weird but its what i come up with in planning)

--- Assets/scripts/Tree.cs
+++ Assets/scripts/Tree.cs
@@ -2,15 +2,31 @@ using UnityEngine;
 
 public class Tree : MonoBehaviour
 {
+    public AnimationCurve curve;
+    public float treeChangingSize;
+    public float speed;
+
+    private Vector3 startSize;
+    private float progress = 0f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        startSize = transform.localScale;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        progress += Time.deltaTime*speed;
+        if (progress > 1f)
+        {
+            progress = 0f;
+        }
+
+        float curveValue = curve.Evaluate(progress);
+        float newSize = 1f + curveValue * treeChangingSize;
+        transform.localScale = startSize * newSize;
+
     }
 }
